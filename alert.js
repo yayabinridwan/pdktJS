f9048ebf1593ebc4539dd6bfb7ef3e291a732f90
Dion Aditya <dionaditya@student.undip.ac.id>
2018-01-06 07:33:54 +0700

    pertama

---
+++ alert.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>PDKT sama JavaScript (1) </h1>
+
+<button onclick="myFunction()">coba diklik</button>
+
+<script>
+var teks = "PDKT sama JS"
+alert(teks); // kita bisa langsung memanggil method 'length' 
+
+
+
+function myFunction() {
+    alert("PDKT sama JS dulu, jangan diganggu");
+}
+
+
+</script>
+
+</body>
+</html>
